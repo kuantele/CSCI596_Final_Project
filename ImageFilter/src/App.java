0184347fc006e7fc2803303057c4d29b9fcd953d
ychen621 <ychen621@usc.edu>
2023-11-21 13:10:52 -0800

    Testing_Single Thread

---
+++ ImageFilter/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ImageFilter imgFilter = new ImageFilter("/Users/ashleychen/Desktop/USC/CSCI 596/ImageFilter/src/photo.png");
+        imgFilter.readImage();
+    }
+}
